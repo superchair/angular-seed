cd12dbe2f3974dc93324ae791a98bf7397199000
abrown <alex.edward.brown@gmail.com>
2014-06-03 15:16:02 -0400

    Started New Dev Meetup Project
    Removed all the crazy seed stuff and just made a simple two way binding
    demo.

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,6 @@
 'use strict';
-(function() {
-    var angularSeed = angular.module('AngularSeed', [
-    ]);
+var angularSeed = angular.module('AngularSeed', []);
 
-    angularSeed.constant('asName', 'Angular Seed');
-    angularSeed.constant('asVersion', '0.0.1');
-
-    angularSeed.controller('asMainCtrl', ['$scope', 'asName', 'asVersion', function($scope, asName, asVersion) {
-        $scope.projectName = asName;
-        $scope.projectVersion = asVersion;
-    }]);
-
-})();
+angularSeed.controller('alexController', function($scope) {
+    $scope.bob = 5;
+});

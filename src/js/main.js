af75718b15a47cf53eb07ec367365f5b3619c018
abrown <alex.edward.brown@gmail.com>
2014-06-10 16:01:51 -0400

    First step into directives
    - how to build a directive
    - what restrict does
    - what template/templateUrl do
    - what controllers do for directives
    - what the link function is (just the beginning)

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,28 @@
 'use strict';
 var angularSeed = angular.module('AngularSeed', []);
 
-angularSeed.controller('alexController', function($scope) {
-    $scope.bob = 5;
+angularSeed.directive('helloName', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/helloName.tmpl.html',
+        replace: true,
+        scope: {
+            color: '@'
+        },
+        controller: function($scope) {
+
+            $scope.onClick = function() {
+                console.log('hi');
+                if($scope.color == 'blue') {
+                    $scope.color = 'red';
+                }
+                else {
+                    $scope.color = 'blue';
+                }
+            };
+        },
+        link: function(scope, elem, attrs) {
+            elem.attr('class', 'testclass');
+        }
+    };
 });
